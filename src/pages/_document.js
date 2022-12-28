5b8e9ed5554be068a94172661b1a1cf1d4224527
yeasin_shamim <yeasinshamim7@gmail.com>
2022-12-28 12:09:29 +0600

    index 3 page done

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,23 +6,14 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
+                    <meta charset="UTF-8" />
+                    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                    <meta name="description" content="Axtra HTML5 Template" />
                     {/* Google Fonts  */}
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
                     <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
-
-                    <script type="text/javascript" src="assets/js/jquery-3.6.0.min.js" async />
-                    <script type="text/javascript" src="assets/js/bootstrap.bundle.min.js" async />
-                    <script type="text/javascript" src="assets/js/swiper-bundle.min.js" async />
-                    <script type="text/javascript" src="assets/js/counter.js" async />
-                    <script type="text/javascript" src="assets/js/gsap.min.js" async />
-                    <script type="text/javascript" src="assets/js/ScrollTrigger.min.js" async />
-                    <script type="text/javascript" src="assets/js/ScrollToPlugin.min.js" async />
-                    <script type="text/javascript" src="assets/js/ScrollSmoother.min.js" async />
-                    <script type="text/javascript" src="assets/js/SplitText.min.js" async />
-                    <script type="text/javascript" src="assets/js/chroma.min.js" async />
-                    <script type="text/javascript" src="assets/js/jquery.meanmenu.min.js" async />
-                    <script type="text/javascript" src="assets/js/main.js" async />
                 </Head>
                 <body>
                     <Main />
--- src/pages/home/content/index-3.jsx
+++ src/pages/home/content/index-3.jsx
@@ -1,29 +1,36 @@
 import Head from "next/head";
-import HomeFooter from "../../../components/home/indexFooter";
-import HeroArea from "../../../components/home/heroArea";
-import BrandArea from "../../../components/home/brandArea";
-import HomeAboutArea from "../../../components/home/homeAboutArea";
-import HomeServiceArea from "../../../components/home/homeServiceArea";
-import HomePortfolioArea from "../../../components/home/homePortfolioArea";
-import HomeTestimonialArea from "../../../components/home/homeTestimonialArea";
-import PriceArea from "../../../components/home/priceArea";
-import WorkflowArea from "../../../components/home/workflowArea";
-import HomeBlog from "../../../components/home/homeBlog";
-import SwitcherArea from "../../../components/common/switcherArea";
-import Offcanvas from "../../../components/common/offcanvas";
-import GoTopButton from "../../../components/common/goTopButton";
-import ScrollSmoother from "../../../components/common/scrollSmoother";
-import IndexHeader from "../../../components/home/indexHeader";
-import Placeholder from "../../../components/common/placeholder";
-import CursorAnimation from "../../../components/common/cursorAnimation";
 import { Fragment, useState } from "react";
+import CtaArea from "../../../components/common/ctaArea";
+import CursorAnimation from "../../../components/common/cursorAnimation";
+import Footer from "../../../components/common/footer";
+import GoTopButton from "../../../components/common/goTopButton";
+import HeaderArea from "../../../components/common/headerArea";
+import Offcanvas from "../../../components/common/offcanvas";
+import Placeholder from "../../../components/common/placeholder";
+import ScrollSmoother from "../../../components/common/scrollSmoother";
+import SwitcherArea from "../../../components/common/switcherArea";
+import BrandArea1 from "../../../components/home/brandArea1";
+import CounterArea from "../../../components/home/counterArea";
+import HeroArea1 from "../../../components/home/heroArea1";
+import HomeAboutArea1 from "../../../components/home/homeAboutArea1";
+import HomeBlog1 from "../../../components/home/homeBlog1";
+import HomeModal from "../../../components/home/homeModal";
+import HomePortfolioArea1 from "../../../components/home/homePortfolioArea1";
+import HomeServiceArea1 from "../../../components/home/homeServiceArea1";
+import HomeTestimonialArea1 from "../../../components/home/homeTestimonialArea1";
+import RollArea from "../../../components/home/rollArea";
+import WorkflowArea1 from "../../../components/home/workflowArea1";
 
-export default function Index1({ mode }) {
+export default function Index3({ mode }) {
     const [bgMode, setBgMode] = useState(mode);
 
     return (
         <Fragment>
             <Head>
+                <meta charset="UTF-8" />
+                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta name="description" content="Axtra HTML5 Template" />
                 {/* Fav Icon  */}
                 <link rel="icon" type="image/x-icon" href="assets/imgs/logo/favicon.png" />
                 <title>Home - Digital Marketing</title>
@@ -69,6 +76,9 @@ export default function Index1({ mode }) {
                 {/* Cursor Animation */}
                 <CursorAnimation />
 
+                {/* Testimonial Play Cursor  */}
+                <div className="cursor" id="client_cursor">Play</div>
+
                 {/* Preloader */}
                 <Placeholder />
 
@@ -83,7 +93,7 @@ export default function Index1({ mode }) {
                 <GoTopButton />
 
                 {/* Header area start */}
-                <IndexHeader />
+                <HeaderArea />
                 {/* Header area end */}
 
                 {/* Offcanvas area start */}
@@ -94,46 +104,54 @@ export default function Index1({ mode }) {
                     <div id="smooth-content">
                         <main>
                             {/* Hero area start */}
-                            <HeroArea />
+                            <HeroArea1 />
                             {/* Hero area start */}
 
-                            {/* Brand area start */}
-                            <BrandArea />
-                            {/* Brand area end */}
+                            {/* Roll area start */}
+                            <RollArea />
+                            {/* Roll area end */}
 
                             {/* About area start */}
-                            <HomeAboutArea />
+                            <HomeAboutArea1 />
                             {/* About area end */}
 
                             {/* Service area start */}
-                            <HomeServiceArea />
+                            <HomeServiceArea1 />
                             {/* Service area end */}
 
-                            {/* Portfolio area start */}
-                            <HomePortfolioArea />
-                            {/* Portfolio area end */}
-
-                            {/* Testimonial area start */}
-                            <HomeTestimonialArea />
-                            {/* Testimonial area end */}
+                            {/* Counter area start */}
+                            <CounterArea />
+                            {/* Counter area end */}
 
                             {/* Workflow area start */}
-                            <WorkflowArea />
+                            <WorkflowArea1 />
                             {/* Workflow area end */}
 
-                            {/* Price area start */}
-                            <PriceArea />
-                            {/* Price area end */}
+                            {/* Portfolio area start */}
+                            <HomePortfolioArea1 />
+                            {/* Portfolio area end */}
+
+                            {/* Brand area start */}
+                            <BrandArea1 />
+                            {/* Brand area end */}
+
+                            {/* Testimonial area start */}
+                            <HomeTestimonialArea1 />
+                            {/* Testimonial area end */}
 
                             {/* Blog area start */}
-                            <HomeBlog />
+                            <HomeBlog1 />
                             {/* Blog area end */}
 
+                            {/* Cta area start */}
+                            <CtaArea />
+                            {/* Cta area end */}
                         </main>
-                        <HomeFooter />
+                        <Footer />
                     </div>
                 </div>
+                <HomeModal />
             </main >
-        </Fragment>
+        </Fragment >
     )
 }

bd7702611270932a75cfb2f249652f64d35be6b7
yeasin_shamim <yeasinshamim7@gmail.com>
2022-12-30 22:53:36 +0600

    link error done

--- src/pages/service/serviceSix.jsx
+++ src/pages/service/serviceSix.jsx
@@ -1,29 +1,29 @@
 import Head from 'next/head';
 import React, { useState } from 'react';
- import CursorAnimation from '../../components/common/cursorAnimation';
+import CursorAnimation from '../../components/common/cursorAnimation';
 import GoTopButton from '../../components/common/goTopButton';
 import Offcanvas from '../../components/common/offcanvas';
 import Placeholder from '../../components/common/placeholder';
 import ScrollSmoother from '../../components/common/scrollSmoother';
- import SwitcherAreaDark from '../../components/common/switcherAreaDark';
- import ServiceHead from '../../components/service/serviceHead';
+import SwitcherAreaDark from '../../components/common/switcherAreaDark';
+import ServiceHead from '../../components/service/serviceHead';
 import ServiceSixCtaArea from '../../components/service/serviceSixCtaArea';
 import ServiceSixFooter from '../../components/service/serviceSixFooter';
 import ServiceSixMain from '../../components/service/serviceSixMain';
 
-const ServiceSix = ({dark}) => {
+const ServiceSix = ({ dark }) => {
     const [stayDark, setStayDark] = useState(true)
     const handleDark = (prop) => {
         setStayDark(prop)
     }
     return (
         <div>
-             <Head>
+            <Head>
                 <meta charset="UTF-8" />
                 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <meta name="description" content="Axtra HTML5 Template" /> 
-                 <title>Service V.6 - Axtra</title> 
+                <meta name="description" content="Axtra HTML5 Template" />
+                <title>Service V.6 - Axtra</title>
 
                 {/* Fav Icon */}
                 <link rel="icon" type="image/x-icon" href="assets/imgs/logo/favicon.png"></link>
@@ -66,13 +66,13 @@ const ServiceSix = ({dark}) => {
                 }
             </Head>
             <body className={dark && stayDark && "dark"}>
-            {/* Cursor Animation */}
-            <CursorAnimation/>
+                {/* Cursor Animation */}
+                <CursorAnimation />
 
-            {/* Preloader */}
-            <Placeholder/>
-            {/* Switcher Area Start  */}
-            <SwitcherAreaDark handleDark={handleDark} dark={dark} />  
+                {/* Preloader */}
+                <Placeholder />
+                {/* Switcher Area Start  */}
+                <SwitcherAreaDark handleDark={handleDark} dark={dark} />
                 {/* Switcher Area End  */}
 
                 {/* Scroll Smoother  */}
@@ -81,7 +81,7 @@ const ServiceSix = ({dark}) => {
                 {/* Go Top Button  */}
                 <GoTopButton />
                 {/* Header area start  */}
-                  <ServiceHead/>
+                <ServiceHead />
                 {/* Header area end  */}
 
                 {/* Offcanvas area start  */}
@@ -92,7 +92,7 @@ const ServiceSix = ({dark}) => {
                     <div id="smooth-content">
                         <main>
                             {/* Service main area start */}
-                            <ServiceSixMain/>
+                            <ServiceSixMain />
                             {/* Service main area start */}
                             {/* CTA area start */}
                             <ServiceSixCtaArea />
@@ -100,7 +100,7 @@ const ServiceSix = ({dark}) => {
                         </main>
 
                         {/* Footer area start */}
-                         <ServiceSixFooter/>
+                        <ServiceSixFooter />
                         {/* Footer area end */}
                     </div>
                 </div>
